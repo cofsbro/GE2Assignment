8e4ddf007879fb6b02c128e1757209d786dea6b1
Cofsbro <cofsbro93@gmail.com>
2020-02-28 01:15:18 +0000

    Idea

---
+++ GE2Assignment/Assets/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public Vector3 camOffset;
+    public float camDistance = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOffset = new Vector3(0, 90, 90);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraTarget.position + camOffset, Time.deltaTime * 5.0f);
+        transform.LookAt(cameraTarget.parent);
+    }
+
+
+}
---
+++ GE2Assignment/Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    Transform gate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gate = GameObject.FindGameObjectWithTag("Gate").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, gate.position, Time.deltaTime * 0.25f);
+        transform.LookAt(gate.parent);
+
+    }
+}
